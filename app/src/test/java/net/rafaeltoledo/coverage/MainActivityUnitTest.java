f2614c6d2ebba4a54bd2e72323978354940bd309
Rafael Toledo <rafaeldtoledo@gmail.com>
2018-02-15 23:23:07 -0200

    Tweaks and tooling update

--- app/src/test/java/net/rafaeltoledo/coverage/MainActivityUnitTest.java
+++ app/src/test/java/net/rafaeltoledo/coverage/MainActivityUnitTest.java
@@ -7,20 +7,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 @RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 25)
 public class MainActivityUnitTest {
 
     @Test
     public void shouldHideButtonAfterClick() {
         MainActivity activity = Robolectric.setupActivity(MainActivity.class);
 
-        Button button = (Button) activity.findViewById(R.id.hide);
+        Button button = activity.findViewById(R.id.hide);
         button.performClick();
 
         assertThat(button.getVisibility(), is(View.GONE));
