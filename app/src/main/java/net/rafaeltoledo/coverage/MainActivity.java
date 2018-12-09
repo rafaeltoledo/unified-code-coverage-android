876c7a1b285299ad0d45261fef47b5194e545ec8
Rafael Toledo <rafaeldtoledo@gmail.com>
2018-12-09 19:29:10 -0200

    Update to AndroidX and AGP 3.3

--- app/src/main/java/net/rafaeltoledo/coverage/MainActivity.java
+++ app/src/main/java/net/rafaeltoledo/coverage/MainActivity.java
@@ -1,10 +1,11 @@
 package net.rafaeltoledo.coverage;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
