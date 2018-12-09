a3ac8ec16dce6d100739b5b7bbe187b4f520632f
Rafael Toledo <rafaeldtoledo@gmail.com>
2018-12-09 19:35:20 -0200

    Updates to AndroidX, Kotlin 1.3 and AGP 3.3

--- app/src/main/java/net/rafaeltoledo/coverage/MainActivity.java
+++ app/src/main/java/net/rafaeltoledo/coverage/MainActivity.java
@@ -1,10 +1,11 @@
 package net.rafaeltoledo.coverage;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         findViewById(R.id.button).setOnClickListener(this);
         findViewById(R.id.hide).setOnClickListener(this);
-        text = (TextView) findViewById(R.id.text);
+        text = findViewById(R.id.text);
     }
 
     @Override
